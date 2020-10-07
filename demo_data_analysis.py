0f52422dc1bfee15bc0db0fde93079725d6bd5bf
JDRValverde <jodaniel09@gmail.com>
2020-10-07 15:14:59 -0500

    adding solution last question

--- demo_data_analysis.py
+++ demo_data_analysis.py
@@ -7,13 +7,15 @@ def calculate_demographic_data(print_data=True):
 
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index
     # labels.
-    race_count = df.groupby('race')['race'].count()
+    race_count = df['race'].value_counts()
 
     # What is the average age of men?
-    average_age_men = round(df[df['sex'] == 'Male']['age'].mean(), 1)
+    filter_ave_age = df[df['sex'] == 'Male']['age'].mean()
+    average_age_men = round(filter_ave_age, 1)
 
     # What is the percentage of people who have a Bachelor's degree?
-    percentage_bachelors = round((df[df['education'] == 'Bachelors'].shape[0] / df.shape[0]) * 100, 1)
+    filter_percent_bachelor = (df[df['education'] == 'Bachelors'].shape[0] / df.shape[0]) * 100
+    percentage_bachelors = round(filter_percent_bachelor, 1)
 
     # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
     # What percentage of people without advanced education make more than 50K?
@@ -23,27 +25,37 @@ def calculate_demographic_data(print_data=True):
     lower_education = df[~df['education'].isin(['Bachelors', 'Masters', 'Doctorate'])]
 
     # percentage with salary >50K
-    higher_education_rich = round(higher_education[higher_education['salary'] == '>50K']['salary'].count() / \
-                            higher_education.shape[0], 2)
-    lower_education_rich = round(lower_education[lower_education['salary'] == '>50K']['salary'].count() / \
-                           lower_education.shape[0], 2)
+    filer_higher_edu = higher_education[higher_education['salary'] == '>50K']['salary'].count() / \
+                       higher_education.shape[0]
+    higher_education_rich = round(filer_higher_edu, 2)
+    filer_lower_edu = lower_education[lower_education['salary'] == '>50K']['salary'].count() / \
+                      lower_education.shape[0]
+    lower_education_rich = round(filer_lower_edu, 2)
 
     # What is the minimum number of hours a person works per week (hours-per-week feature)?
     min_work_hours = df['hours-per-week'].min()
 
     # What percentage of the people who work the minimum number of hours per week have a salary of >50K?
-    num_min_workers = round(df[df['hours-per-week'] == 1]['hours-per-week'].count(), 1)
+    filter_num_min_workers = df[df['hours-per-week'] == 1]['hours-per-week'].count()
+    num_min_workers = round(filter_num_min_workers, 1)
 
-    rich_percentage = round(df[(df['hours-per-week'] == 1) & (df['salary'] == '>50K')].shape[0] / num_min_workers, 1)
+    filter_rich_percentage = df[(df['hours-per-week'] == 1) & (df['salary'] == '>50K')].shape[0] / num_min_workers
+    rich_percentage = round(filter_rich_percentage, 1)
 
     # What country has the highest percentage of people that earn >50K?
-    highest_earning_country = df[df['salary'] == '>50K'].groupby('native-country')['native-country'].count().max()
-    highest_earning_country_percentage = (highest_earning_country / df.groupby('native-country')[
-        'native-country'].count()).max() * 100
+    # BeauCarnes
+    highest_earning_country = ""
+    highest_earning_country_percentage = 0
+    for country, sub_df in df.groupby(['native-country']):
+        percentage = len(sub_df[(sub_df['salary'] == '>50K')]) / len(sub_df)
+        if highest_earning_country_percentage < percentage:
+            highest_earning_country_percentage = round(percentage, 3)
+            highest_earning_country = country
+    highest_earning_country_percentage *= 100
 
     # Identify the most popular occupation for those who earn >50K in India.
-    top_IN_occupation = df[(df['salary'] == '>50K') & (df['native-country'] == 'India')].groupby('occupation')[
-        'occupation'].count().max()
+    top_IN_occupation = df[(df['salary'] == '>50K') & (df['native-country'] == 'India')].loc[:,
+                        'occupation'].value_counts()[:1].sort_values(ascending=False)
 
     # DO NOT MODIFY BELOW THIS LINE
 
@@ -75,3 +87,7 @@ def calculate_demographic_data(print_data=True):
 
 
 calculate_demographic_data()
+
+
+# REFEREBCES
+# 1. BeauCarnes
