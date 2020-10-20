33fef6e7559492bb0d7a84e863a054c74b3590a2
JDRValverde <jodaniel09@gmail.com>
2020-10-20 11:25:42 -0500

    Adding README.md

--- test.py
+++ test.py
@@ -1,8 +1,93 @@
 import pandas as pd
 
-df = pd.read_csv("adult.data.csv")
-df.head()
-df.race.unique()
-df.race.value_counts().unique()
 
-#print(round((df[df['education'] == 'Bachelors'].shape[0] / df.shape[0]) * 100))
+def calculate_demographic_data(print_data=True):
+    # Read data from file
+    df = pd.read_csv("adult.data.csv")
+
+    # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index
+    # labels.
+    race_count = df['race'].value_counts()
+
+    # What is the average age of men?
+    filter_ave_age = df[df['sex'] == 'Male']['age'].mean()
+    average_age_men = round(filter_ave_age, 1)
+
+    # What is the percentage of people who have a Bachelor's degree?
+    filter_percent_bachelor = (df[df['education'] == 'Bachelors'].shape[0] / df.shape[0]) * 100
+    percentage_bachelors = round(filter_percent_bachelor, 1)
+
+    # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
+    # What percentage of people without advanced education make more than 50K?
+
+    # with and without `Bachelors`, `Masters`, or `Doctorate`
+    higher_education = df[df['education'].isin(['Bachelors', 'Masters', 'Doctorate'])]
+    lower_education = df[~df['education'].isin(['Bachelors', 'Masters', 'Doctorate'])]
+
+    # percentage with salary >50K
+    filer_higher_edu = higher_education[higher_education['salary'] == '>50K']['salary'].count() / \
+                       higher_education.shape[0]
+    higher_education_rich = round(filer_higher_edu, 2)
+    filer_lower_edu = lower_education[lower_education['salary'] == '>50K']['salary'].count() / \
+                      lower_education.shape[0]
+    lower_education_rich = round(filer_lower_edu, 2)
+
+    # What is the minimum number of hours a person works per week (hours-per-week feature)?
+    min_work_hours = df['hours-per-week'].min()
+
+    # What percentage of the people who work the minimum number of hours per week have a salary of >50K?
+    filter_num_min_workers = df[df['hours-per-week'] == 1]['hours-per-week'].count()
+    num_min_workers = round(filter_num_min_workers, 1)
+
+    filter_rich_percentage = df[(df['hours-per-week'] == 1) & (df['salary'] == '>50K')].shape[0] / num_min_workers
+    rich_percentage = round(filter_rich_percentage, 1)
+
+    # What country has the highest percentage of people that earn >50K?
+    # BeauCarnes
+    highest_earning_country = ""
+    highest_earning_country_percentage = 0
+    for country, sub_df in df.groupby(['native-country']):
+        percentage = len(sub_df[(sub_df['salary'] == '>50K')]) / len(sub_df)
+        if highest_earning_country_percentage < percentage:
+            highest_earning_country_percentage = round(percentage, 3)
+            highest_earning_country = country
+    highest_earning_country_percentage *= 100
+
+    # Identify the most popular occupation for those who earn >50K in India.
+    top_IN_occupation = df[(df['salary'] == '>50K') & (df['native-country'] == 'India')].loc[:,
+                        'occupation'].value_counts()[:1].sort_values(ascending=False)
+
+    # DO NOT MODIFY BELOW THIS LINE
+
+    if print_data:
+        print("Number of each race:\n", race_count)
+        print("Average age of men:", average_age_men)
+        print(f"Percentage with Bachelors degrees: {percentage_bachelors}%")
+        print(f"Percentage with higher education that earn >50K: {higher_education_rich}%")
+        print(f"Percentage without higher education that earn >50K: {lower_education_rich}%")
+        print(f"Min work time: {min_work_hours} hours/week")
+        print(f"Percentage of rich among those who work fewest hours: {rich_percentage}%")
+        print("Country with highest percentage of rich:", highest_earning_country)
+        print(f"Highest percentage of rich people in country: {highest_earning_country_percentage}%")
+        print("Top occupations in India:", top_IN_occupation)
+
+    return {
+        'race_count': race_count,
+        'average_age_men': average_age_men,
+        'percentage_bachelors': percentage_bachelors,
+        'higher_education_rich': higher_education_rich,
+        'lower_education_rich': lower_education_rich,
+        'min_work_hours': min_work_hours,
+        'rich_percentage': rich_percentage,
+        'highest_earning_country': highest_earning_country,
+        'highest_earning_country_percentage':
+            highest_earning_country_percentage,
+        'top_IN_occupation': top_IN_occupation
+    }
+
+
+calculate_demographic_data()
+
+
+# REFEREBCES
+# 1. BeauCarnes
